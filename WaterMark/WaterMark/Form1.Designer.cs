8ec48d3e0a48faf9ddbd306bee058f90a7ef31de
PLaGInc <vladimir.alt@inbox.ru>
2015-07-17 09:31:51 +0400

    Готово

---
+++ WaterMark/WaterMark/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WaterMark
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Middle = new System.Windows.Forms.RadioButton();
+            this.Multiply = new System.Windows.Forms.RadioButton();
+            this.Stretch = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenImage = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenWaterMark = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Mark = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Middle
+            // 
+            this.Middle.AutoSize = true;
+            this.Middle.Location = new System.Drawing.Point(3, 3);
+            this.Middle.Name = "Middle";
+            this.Middle.Size = new System.Drawing.Size(56, 17);
+            this.Middle.TabIndex = 2;
+            this.Middle.TabStop = true;
+            this.Middle.Text = "Middle";
+            this.Middle.UseVisualStyleBackColor = true;
+            this.Middle.CheckedChanged += new System.EventHandler(this.Middle_CheckedChanged);
+            // 
+            // Multiply
+            // 
+            this.Multiply.AutoSize = true;
+            this.Multiply.Location = new System.Drawing.Point(3, 26);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(60, 17);
+            this.Multiply.TabIndex = 3;
+            this.Multiply.TabStop = true;
+            this.Multiply.Text = "Multiply";
+            this.Multiply.UseVisualStyleBackColor = true;
+            this.Multiply.CheckedChanged += new System.EventHandler(this.Multiply_CheckedChanged);
+            // 
+            // Stretch
+            // 
+            this.Stretch.AutoSize = true;
+            this.Stretch.Location = new System.Drawing.Point(3, 49);
+            this.Stretch.Name = "Stretch";
+            this.Stretch.Size = new System.Drawing.Size(59, 17);
+            this.Stretch.TabIndex = 4;
+            this.Stretch.TabStop = true;
+            this.Stretch.Text = "Stretch";
+            this.Stretch.UseVisualStyleBackColor = true;
+            this.Stretch.CheckedChanged += new System.EventHandler(this.Stretch_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.Stretch);
+            this.panel1.Controls.Add(this.Middle);
+            this.panel1.Controls.Add(this.Multiply);
+            this.panel1.Location = new System.Drawing.Point(458, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(101, 76);
+            this.panel1.TabIndex = 5;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.infoToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(567, 24);
+            this.menuStrip1.TabIndex = 6;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenImage,
+            this.OpenWaterMark,
+            this.saveAsToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.openFileToolStripMenuItem.Text = "File";
+            // 
+            // OpenImage
+            // 
+            this.OpenImage.Name = "OpenImage";
+            this.OpenImage.Size = new System.Drawing.Size(171, 22);
+            this.OpenImage.Text = "Open image...";
+            this.OpenImage.Click += new System.EventHandler(this.openFileToolStripMenuItem1_Click);
+            // 
+            // OpenWaterMark
+            // 
+            this.OpenWaterMark.Name = "OpenWaterMark";
+            this.OpenWaterMark.Size = new System.Drawing.Size(171, 22);
+            this.OpenWaterMark.Text = "Open watermark...";
+            this.OpenWaterMark.Click += new System.EventHandler(this.openImageToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.helpToolStripMenuItem.Text = "Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(440, 312);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Mark
+            // 
+            this.Mark.AutoSize = true;
+            this.Mark.Font = new System.Drawing.Font("Algerian", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Mark.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Mark.Location = new System.Drawing.Point(458, 326);
+            this.Mark.Name = "Mark";
+            this.Mark.Size = new System.Drawing.Size(101, 12);
+            this.Mark.TabIndex = 8;
+            this.Mark.Text = "Created by PLaG";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 351);
+            this.Controls.Add(this.Mark);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WaterMark";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton Middle;
+        private System.Windows.Forms.RadioButton Multiply;
+        private System.Windows.Forms.RadioButton Stretch;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem OpenImage;
+        private System.Windows.Forms.ToolStripMenuItem OpenWaterMark;
+        private System.Windows.Forms.ToolStripMenuItem infoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label Mark;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ WaterMark/WaterMark/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace WaterMark
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap original, watermark;
+        private bool mid = false, mul = false, stret = false;
+        string originalName, watermarkName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1.Load image file\n2.Load watermark file\n3.Press button\n", "Help");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program was created for the summer practice.\nAuthor: Alitov Vladimir(PLaG)\nGroup: PS-21\n", "About");
+        }
+
+        private void openFileToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image files|*.bmp";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                originalName = openFileDialog1.FileName;
+                original = new Bitmap(originalName);
+                if (watermark != null)
+                {
+                    if (mid)
+                    {
+                        MiddleProc();
+                        mid = false;
+                    }
+                    else if (mul)
+                    {
+                        MultProc();
+                        mul = false;
+                    }
+                    else if (stret)
+                    {
+                        StretProc();
+                        stret = false;
+                    }
+                }
+                else
+                {
+                    pictureBox1.Image = original;
+                }
+            }
+        }
+
+        private void openImageToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image files (*.bmp)|*.bmp|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                watermarkName = openFileDialog1.FileName;
+                watermark = new Bitmap(watermarkName);
+                if (original != null)
+                {
+                    if (mid)
+                    {
+                        MiddleProc();
+                        mid = false;
+                    }
+                    else if (mul)
+                    {
+                        MultProc();
+                        mul = false;
+                    }
+                    else if (stret)
+                    {
+                        StretProc();
+                        stret = false;
+                    }
+                }
+                else
+                {
+                    pictureBox1.Image = watermark;
+                }
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("No picture!");
+                return;
+            }
+            Bitmap bmpSave = new Bitmap(pictureBox1.Image);
+            if (bmpSave != null)
+            {
+                bmpSave.Save(openFileDialog1.FileName, ImageFormat.Bmp);
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+            {
+                MessageBox.Show("No picture!");
+                return;
+            }
+            Bitmap bmpSave = new Bitmap(pictureBox1.Image);
+            if (bmpSave != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.DefaultExt = "bmp";
+                sfd.Filter = "Image files (*.bmp)|*.bmp|All files (*.*)|*.*";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                    bmpSave.Save(sfd.FileName, ImageFormat.Bmp);
+            }
+        }    
+        
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        static float SoftLight(int a, int b)
+        {
+            float newA = a / 255.0f, newB = b / 255.0f;
+            return newB < 0.5f ? Math.Min(1, (0.5f - newB) * newA + 2 * newB * newA + newA * newA * 
+                (1 - 2 * newB)) : 2 * newA * (1 - newB) + Convert.ToSingle((Math.Sqrt(newA))) * (2 * newB - 1); 
+        }
+
+        private void MiddleProc()
+        {
+            if (original == null && mid)
+            {
+                MessageBox.Show("No picture!");
+                return;
+            }
+            if (watermark == null && mid)
+            {
+                MessageBox.Show("No watermark!");
+                return;
+            }
+            mid = true;
+            mul = false;
+            stret = false;
+            if (watermark != null && original != null)
+            {
+                int oldWidth = watermark.Width, oldHeight = watermark.Height;
+                if (watermark.Width > original.Width && watermark.Height > original.Height)
+                {
+                    watermark = new Bitmap(Image.FromFile(watermarkName), original.Width, original.Height);
+                }
+                Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+                int centerX = Math.Max(0, (original.Width - watermark.Width) / 2);
+                int centerY = Math.Max(0, (original.Height - watermark.Height) / 2);
+                float kx = 1.0f * original.Width / watermark.Width,
+                    ky = 1.0f * original.Height / watermark.Height;
+
+                for (int i = 0; i < original.Width; ++i)
+                {
+                    for (int j = 0; j < original.Height; ++j)
+                    {
+                        var inpPixel = original.GetPixel(i, j);
+                        if (i >= centerX && j >= centerY && i < centerX + watermark.Width && j < centerY + watermark.Height)
+                        {
+                            var watermarkPixel = watermark.GetPixel(i - centerX, j - centerY);
+                            float bright = 1 - watermarkPixel.A / 255.0f;
+                            Color newColor = Color.FromArgb((int)(255 * SoftLight(inpPixel.R, (int)(watermarkPixel.R + (inpPixel.R - watermarkPixel.R) * bright))),
+                                (int)(255 * SoftLight(inpPixel.G, (int)(watermarkPixel.G + (inpPixel.G - watermarkPixel.G) * bright))),
+                                (int)(255 * SoftLight(inpPixel.B, (int)(watermarkPixel.B + (inpPixel.B - watermarkPixel.B) * bright))));
+
+                            newBitmap.SetPixel(i, j, newColor);
+                        }
+                        else
+                        {
+                            newBitmap.SetPixel(i, j, inpPixel);
+                        }
+                    }
+                }
+                watermark = new Bitmap(Image.FromFile(watermarkName), oldWidth, oldHeight);
+                pictureBox1.Image = newBitmap;
+            }
+        }
+
+        private void MultProc()
+        { 
+            if (original == null && mul)
+            {
+                MessageBox.Show("No picture!");
+                return;
+            }
+            if (watermark == null && mul)
+            {
+                MessageBox.Show("No watermark!");
+                return;
+            }
+            mul = true;
+            mid = false;
+            stret = false;
+            if (watermark != null && original != null)
+            {
+                int oldWidth = watermark.Width, oldHeight = watermark.Height;
+                if (watermark.Width > original.Width && watermark.Height > original.Height)
+                {
+                    watermark = new Bitmap(Image.FromFile(watermarkName), original.Width / 2, original.Height / 2);
+                }
+
+                Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+                int centerX = Math.Max(0, (original.Width - watermark.Width) / 2);
+                int centerY = Math.Max(0, (original.Height - watermark.Height) / 2);
+                float kx = 1.0f * original.Width / watermark.Width,
+                    ky = 1.0f * original.Height / watermark.Height;
+
+                for (int i = 0; i < original.Width; ++i)
+                {
+                    for (int j = 0; j < original.Height; ++j)
+                    {
+                        var inpPixel = original.GetPixel(i, j);
+                        var watermarkPixel = watermark.GetPixel(i % watermark.Width, j % watermark.Height);
+                        float bright = 1 - watermarkPixel.A / 255.0f;
+                        Color newColor = Color.FromArgb((int)(255 * SoftLight(inpPixel.R, (int)(watermarkPixel.R + (inpPixel.R - watermarkPixel.R) * bright))),
+                            (int)(255 * SoftLight(inpPixel.G, (int)(watermarkPixel.G + (inpPixel.G - watermarkPixel.G) * bright))),
+                            (int)(255 * SoftLight(inpPixel.B, (int)(watermarkPixel.B + (inpPixel.B - watermarkPixel.B) * bright))));
+
+                        newBitmap.SetPixel(i, j, newColor);
+                    }
+                }
+                pictureBox1.Image = newBitmap;
+                watermark = new Bitmap(Image.FromFile(watermarkName), oldWidth, oldHeight);
+            }
+        }
+
+        private void StretProc()
+        {
+            
+            if (original == null && stret)
+            {
+                MessageBox.Show("No picture!");
+                return;
+            }
+            if (watermark == null && stret)
+            {
+                MessageBox.Show("No watermark!");
+                return;
+            }
+            stret = true;
+            mul = false;
+            mid = false;
+            if (watermark != null && original != null)
+            {
+                Bitmap newBitmap = new Bitmap(original.Width, original.Height);
+                int centerX = Math.Max(0, (original.Width - watermark.Width) / 2);
+                int centerY = Math.Max(0, (original.Height - watermark.Height) / 2);
+                float kx = 1.0f * original.Width / watermark.Width, ky = 1.0f * original.Height / watermark.Height;
+
+                for (int i = 0; i < original.Width; ++i)
+                {
+                    for (int j = 0; j < original.Height; ++j)
+                    {
+                        var inpPixel = original.GetPixel(i, j);
+                        var watermarkPixel = watermark.GetPixel((int)(i / kx), (int)(j / ky));
+                        float bright = 1 - watermarkPixel.A / 255.0f;
+                        Color newColor = Color.FromArgb((int)(255 * SoftLight(inpPixel.R, (int)(watermarkPixel.R + (inpPixel.R - watermarkPixel.R) * bright))),
+                            (int)(255 * SoftLight(inpPixel.G, (int)(watermarkPixel.G + (inpPixel.G - watermarkPixel.G) * bright))),
+                            (int)(255 * SoftLight(inpPixel.B, (int)(watermarkPixel.B + (inpPixel.B - watermarkPixel.B) * bright))));
+
+                        newBitmap.SetPixel(i, j, newColor);
+                    }
+                }
+                pictureBox1.Image = newBitmap;
+            }
+        }
+
+        private void Middle_CheckedChanged(object sender, EventArgs e)
+        {
+            MiddleProc();
+        }
+
+        private void Multiply_CheckedChanged(object sender, EventArgs e)
+        {
+            MultProc();
+        }
+
+        private void Stretch_CheckedChanged(object sender, EventArgs e)
+        {
+            StretProc();
+        }
+
+    }
+}
+
